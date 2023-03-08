1f95413299364d2144ba651b37f43d6dc2d45951
khangng2611 <khang.nguyenphuc2611@hcmut.edu.vn>
2023-03-09 02:48:25 +0700

    RoutesControllers

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ const { engine } = require ('express-handlebars');
 const app = express()
 const port = 3000
 
+const route = require('./routes/index')
+
 //Static file
 app.use(express.static(path.join(__dirname, 'public')))
+
+//middleware (html form)
+app.use(express.urlencoded())
+//middleware (code js)
+app.use(express.json())
 
 //HTTP logger
 app.use(morgan('combined'))
@@ -19,13 +26,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//Routes init
+route(app)
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
